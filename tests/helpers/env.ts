20d6df142b3ea6582f37642db50d7f7272eb115a
anlyton <44033719+anlyton@users.noreply.github.com>
2026-04-09 19:54:28 +0200

    fix tests

--- tests/helpers/env.ts
+++ tests/helpers/env.ts
@@ -1,4 +1,4 @@
 export const TEST_USER = {
-  email:    process.env.TEST_EMAIL    ?? 'admin@aqa.dev',
-  password: process.env.TEST_PASSWORD ?? 'aqa2024',
+  email:    process.env.TEST_EMAIL    || 'admin@aqa.dev',
+  password: process.env.TEST_PASSWORD || 'aqa2024',
 }
